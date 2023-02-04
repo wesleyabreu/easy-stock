8747eedfe95f64337e2c513448bcb37a25ea5b53
gZanda <gabrielfzanda@gmail.com>
2023-02-04 06:07:59 -0300

    Tela inicial = Login, Link "cadastre-se" funciona

--- Complete Code/stock/src/main/java/com/easy/stock/controller/Autenticacao.java
+++ Complete Code/stock/src/main/java/com/easy/stock/controller/Autenticacao.java
@@ -6,39 +6,39 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.easy.stock.model.Usuario;
 import com.easy.stock.repository.Dao;
 
 // Controller da autenticação de usuário
-// HTML related: cadastro-cliente
+// HTML related: cadastro-cliente, login
 
 @Controller
 public class Autenticacao {  
     
     @Autowired
-    private Dao dao;
+    private Dao dao;    // DAO =  DataBase Acess Object ( é através desse objeto que usamos funções do Repositório CRUD )
     
-    @GetMapping("/cadastro") // URL root "/" básica, vai carregar a "index" page HTML - Root, página font
+    @GetMapping("/") // URL root "/" básica, vai carregar a "login" page HTML - Root, página fonte
     public String indexPage(){
-        return "cadastro-cliente"; // Esse return identifica o diretório "templates"
+        return "login"; // Esse return identifica o diretório "templates"
     }
 
-    @PostMapping("/register")
+    // Direcionar o usuário para o cadastro, caso ele não tenha 
+    @RequestMapping("/redirect")    // Endpoit que faz ação de redirecionar o cliente para o CADASTRO ( caso ele não tenha )
+    public String userRedirect(){
+        return "cadastro-cliente";
+    }
+
+    // Registrar usuário - CLIENTE
+    @PostMapping("/cadastrar")  // Endepoint que faz ação de CADASTRAR o cliente
     public String userRegistration(@ModelAttribute Usuario usuario, Model model){
 
-        // model.addAttribute("name", user.getName()); // Coloca no objeto "name" o valor desse atributo
-
-        usuario.setTipo_conta("Cliente");
-
+        usuario.setTipo_conta("Cliente");   // Adiciona o atributo que o usuário não define ( seu tipo de conta)
         dao.save(usuario);
-        //model.addAttribute("message", userInsert.getEmail());
 
         return "login";   // Onde vai após o cadastro
     }
-
-
-
-    
 
 }
--- Complete Code/stock/src/main/java/com/easy/stock/repository/Dao.java
+++ Complete Code/stock/src/main/java/com/easy/stock/repository/Dao.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.easy.stock.model.Usuario;
 
+// Interface que extende o repositório CRUD do java SPRING
+
 public interface Dao extends CrudRepository<Usuario, String> {
     
 }
